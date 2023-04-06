e14ebe4c6624f13733b8b28b1543c211abfa2a9b
Rojan Baral <baralrojan088@gmail.com>
2023-04-06 00:39:22 -0400

    1)Created two models, BlogPost and Tag, with many to many relationships between BlogPost and Tag. 2)Created BlogDbContext to access and store data in our database. 3)Injected BlogDbContext in the program. cs to get the connection from the specified connection string and injected that into the service of the application. 3)Created AdminTagsController inside the controller, with Add action method which allows the admin to add a tag. 4)Created a form to add tags in Add. cshtml inside the AdminTags folder of views. 5)Model Binding- To read the data from the form and capture it inside the controller Model Binding was Used. 6)To store form data, HttpPost action named Add with the parameter AddTagRequest was created inside the AdminTagsController. Inside the method, the name and displayName properties of the addTagRequest object are assigned to local variables. Mapping AddTagRequest to Tag domain model was done. 7) Using DbContext class, name, and displayName values were saved to the database. 8) New method named List was created inside the controller. 9) The ToList() method is called on the Tags property of the blogDbContext object, which returns a collection of Tag objects from the database.

---
+++ DevBlog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,50 @@
+﻿using DevBlog.Web.Data;
+using DevBlog.Web.Models.Domain;
+using DevBlog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBlog.Web.Controllers
+{
+
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogDbContext blogDbContext;
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext= blogDbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            blogDbContext.Tags.Add(tag);
+            //Context save to database
+            blogDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List() 
+        {
+            //use dbContext to read the tags
+            var tags = blogDbContext.Tags.ToList();
+            return View(tags);
+        }
+    }
+}
