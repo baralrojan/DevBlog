25b78c1630a80119770ee0c36a602def07710148
Rojan Baral <baralrojan088@gmail.com>
2023-04-14 09:43:29 -0400

    # Added a neww form to add blog infornation.

---
+++ DevBlog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,33 @@
+﻿using DevBlog.Library.Models.ViewModels;
+using DevBlog.Library.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevBlog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagServices tagServices;
+        //private readonly IBlogPostRepository blogPostRepository;
+
+        public AdminBlogPostsController(ITagServices tagServices)
+        {
+            this.tagServices = tagServices;
+            //this.blogPostRepository = blogPostRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            // get tags from repository
+            var tags = await tagServices.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+    }
+}
